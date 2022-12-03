2abcf072f921b658c64a305e8b84bbcb07d8a46a
Tristan Guichaoua <tguichaoua.dev@gmail.com>
2022-12-03 08:03:09 +0100

    🎄Day 03

---
+++ src/bin/03.rs
@@ -0,0 +1,168 @@
+use std::collections::HashSet;
+
+use adventofcode::helpers::StrExt;
+use itertools::Itertools;
+
+#[inline]
+fn item_priority(item: u8) -> u32 {
+    match item {
+        x @ b'a'..=b'z' => (x - b'a' + 1) as u32,
+        x @ b'A'..=b'Z' => (x - b'A' + 27) as u32,
+        _ => unreachable!(),
+    }
+}
+
+pub fn part_one(input: &str) -> Option<u32> {
+    let value = input
+        .lines()
+        .map(|line| {
+            let item_count = line.len();
+            assert_eq!(item_count % 2, 0);
+            let (a, b) = line.split_at(item_count / 2);
+            assert_eq!(a.len(), b.len());
+            let a = HashSet::<_>::from_iter(a.as_bytes());
+            let b = HashSet::from_iter(b.as_bytes());
+
+            let item = a.intersection(&b).exactly_one().unwrap();
+
+            item_priority(**item)
+        })
+        .sum();
+
+    Some(value)
+}
+
+pub fn part_one_2(input: &str) -> Option<u32> {
+    let value = input
+        .lines()
+        .map(|line| {
+            let item_count = line.len();
+            assert_eq!(item_count % 2, 0);
+            let mid = item_count / 2;
+            // SAFETY: `mid` is on a char boundary : the input only contains
+            // 1 byte length character ('a' through 'z' and 'A' through 'Z');
+            let (a, b) = unsafe { line.split_at_unchecked(mid) };
+            assert_eq!(a.len(), b.len());
+            let a = HashSet::<_>::from_iter(a.as_bytes());
+            let b = HashSet::from_iter(b.as_bytes());
+
+            let item = a.intersection(&b).exactly_one().unwrap();
+
+            item_priority(**item)
+        })
+        .sum();
+
+    Some(value)
+}
+
+pub fn part_one_3(input: &str) -> Option<u32> {
+    let value = input
+        .lines()
+        .map(|line| {
+            let item_count = line.len();
+            assert_eq!(item_count % 2, 0);
+            let mid = item_count / 2;
+            // SAFETY: `mid` is on a char boundary : the input only contains
+            // 1 byte length character ('a' through 'z' and 'A' through 'Z');
+            let (a, b) = unsafe { line.split_at_unchecked(mid) };
+            assert_eq!(a.len(), b.len());
+            let a = HashSet::<_>::from_iter(a.as_bytes().iter().copied());
+            let b = HashSet::from_iter(b.as_bytes().iter().copied());
+
+            let item = a.intersection(&b).exactly_one().unwrap();
+
+            item_priority(*item)
+        })
+        .sum();
+
+    Some(value)
+}
+
+pub fn part_two(input: &str) -> Option<u32> {
+    let value = input
+        .lines()
+        .tuples()
+        .map(|(a, b, c)| {
+            let a = HashSet::<_>::from_iter(a.as_bytes());
+            let b = HashSet::from_iter(b.as_bytes());
+            let c = HashSet::from_iter(c.as_bytes());
+
+            let a_b_common = a.intersection(&b).copied().collect::<HashSet<_>>();
+            let common_item = a_b_common.intersection(&c).exactly_one().unwrap();
+
+            item_priority(**common_item)
+        })
+        .sum();
+
+    Some(value)
+}
+
+pub fn part_two_2(input: &str) -> Option<u32> {
+    let value = input
+        .lines()
+        .tuples()
+        .map(|(a, b, c)| {
+            let a = HashSet::<_>::from_iter(a.as_bytes().iter().copied());
+            let b = HashSet::from_iter(b.as_bytes().iter().copied());
+            let c = HashSet::from_iter(c.as_bytes().iter().copied());
+
+            let a_b_common = a.intersection(&b).copied().collect::<HashSet<_>>();
+            let common_item = a_b_common.intersection(&c).exactly_one().unwrap();
+
+            item_priority(*common_item)
+        })
+        .sum();
+
+    Some(value)
+}
+
+fn main() {
+    let input = &adventofcode::read_file("inputs", 3);
+    {
+        // HashSet does some stuff when used for the first time
+        // that took ~300 µs (on my machine).
+        // Create a HashSet here to not impact the solutions performance.
+        let mut dummy = HashSet::with_capacity(1);
+        dummy.insert(0u8);
+    }
+    // adventofcode::solve!(1, part_one, input);
+    // adventofcode::solve!(1, part_one_2, input);
+    adventofcode::solve!(1, part_one_3, input);
+    // adventofcode::solve!(2, part_two, input);
+    adventofcode::solve!(2, part_two_2, input);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part_one() {
+        let input = adventofcode::read_file("examples", 3);
+        assert_eq!(part_one(&input), Some(157));
+    }
+
+    #[test]
+    fn test_part_one_2() {
+        let input = adventofcode::read_file("examples", 3);
+        assert_eq!(part_one_2(&input), Some(157));
+    }
+
+    #[test]
+    fn test_part_one_3() {
+        let input = adventofcode::read_file("examples", 3);
+        assert_eq!(part_one_3(&input), Some(157));
+    }
+
+    #[test]
+    fn test_part_two() {
+        let input = adventofcode::read_file("examples", 3);
+        assert_eq!(part_two(&input), Some(70));
+    }
+
+    #[test]
+    fn test_part_two_2() {
+        let input = adventofcode::read_file("examples", 3);
+        assert_eq!(part_two_2(&input), Some(70));
+    }
+}
--- src/helpers.rs
+++ src/helpers.rs
@@ -15,3 +15,38 @@ impl<T: PartialEq> VecExt<T> for Vec<T> {
             .map(|index| self.remove(index))
     }
 }
+
+pub trait StrExt {
+    /// Divide one string slice into two at an index, without doing bounds checking.
+    ///
+    /// The argument, `mid`, should be a byte offset from the start of the
+    /// string. It must also be on the boundary of a UTF-8 code point.
+    ///
+    /// The two slices returned go from the start of the string slice to `mid`,
+    /// and from `mid` to the end of the string slice.
+    ///
+    /// # Safety
+    ///
+    /// Callers of this function are responsible that these preconditions are
+    /// satisfied:
+    ///
+    /// * `mid` must be within bounds of the string slice;
+    /// * `mid` must lie on UTF-8 sequence boundaries.
+    ///
+    /// Failing that, the returned string slice may reference invalid memory or
+    /// violate the invariants communicated by the `str` type.
+    unsafe fn split_at_unchecked(&self, mid: usize) -> (&str, &str);
+}
+
+impl<'a> StrExt for &'a str {
+    #[inline]
+    unsafe fn split_at_unchecked(&self, mid: usize) -> (&str, &str) {
+        // SAFETY: the caller must uphold the safety contract for `get_unchecked`;
+        unsafe {
+            (
+                self.get_unchecked(0..mid),
+                self.get_unchecked(mid..self.len()),
+            )
+        }
+    }
+}
