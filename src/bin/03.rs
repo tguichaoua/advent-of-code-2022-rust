8d9aa948a07de7870d5449cb048802133dc60c8f
Tristan Guichaoua <tguichaoua.dev@gmail.com>
2022-12-09 14:24:44 +0100

    ➕ byte_set

--- src/bin/03.rs
+++ src/bin/03.rs
@@ -3,6 +3,7 @@
 use std::collections::HashSet;
 
 use adventofcode::helpers::StrExt;
+use byte_set::ByteSet;
 use itertools::Itertools;
 
 #[inline]
@@ -82,6 +83,32 @@ pub fn part_one_3(input: &str) -> Option<u32> {
     Some(value)
 }
 
+pub fn part_one_4(input: &str) -> Option<u32> {
+    let value = input
+        .lines()
+        .map(|line| {
+            let item_count = line.len();
+            debug_assert_eq!(item_count % 2, 0);
+            let mid = item_count / 2;
+            // SAFETY: `mid` is on a char boundary : the input only contains
+            // 1 byte length character ('a' through 'z' and 'A' through 'Z');
+            let (a, b) = unsafe { line.split_at_unchecked(mid) };
+            debug_assert_eq!(a.len(), b.len());
+
+            let a = ByteSet::from_iter(a.bytes());
+            let b = ByteSet::from_iter(b.bytes());
+
+            let intersection = a.intersection(b);
+            debug_assert_eq!(intersection.len(), 1);
+            let item = intersection.first().unwrap();
+
+            item_priority(item)
+        })
+        .sum();
+
+    Some(value)
+}
+
 pub fn part_two(input: &str) -> Option<u32> {
     let value = input
         .lines()
@@ -120,20 +147,42 @@ pub fn part_two_2(input: &str) -> Option<u32> {
     Some(value)
 }
 
+pub fn part_two_3(input: &str) -> Option<u32> {
+    let value = input
+        .lines()
+        .tuples()
+        .map(|(a, b, c)| {
+            let a = ByteSet::from_iter(a.bytes());
+            let b = ByteSet::from_iter(b.bytes());
+            let c = ByteSet::from_iter(c.bytes());
+
+            let common_item = a.intersection(b).intersection(c);
+            debug_assert_eq!(common_item.len(), 1);
+            let item = common_item.first().unwrap();
+
+            item_priority(item)
+        })
+        .sum();
+
+    Some(value)
+}
+
 fn main() {
     let input = &adventofcode::read_file("inputs", 3);
-    {
-        // HashSet does some stuff when used for the first time
-        // that took ~300 µs (on my machine).
-        // Create a HashSet here to not impact the solutions performance.
-        let mut dummy = HashSet::with_capacity(1);
-        dummy.insert(0u8);
-    }
+    // {
+    //     // HashSet does some stuff when used for the first time
+    //     // that took ~300 µs (on my machine).
+    //     // Create a HashSet here to not impact the solutions performance.
+    //     let mut dummy = HashSet::with_capacity(1);
+    //     dummy.insert(0u8);
+    // }
     // adventofcode::solve!(1, part_one, input);
     // adventofcode::solve!(1, part_one_2, input);
-    adventofcode::solve!(1, part_one_3, input);
+    // adventofcode::solve!(1, part_one_3, input);
+    adventofcode::solve!(1, part_one_4, input);
     // adventofcode::solve!(2, part_two, input);
-    adventofcode::solve!(2, part_two_2, input);
+    // adventofcode::solve!(2, part_two_2, input);
+    adventofcode::solve!(2, part_two_3, input);
 }
 
 #[cfg(test)]
